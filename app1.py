0788b7989a80c42de4554d0347e47b364743043d
Nikhil Padamtintiwar <nikhilsantosh2206@gmail.com>
2026-05-19 16:30:52 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -39,6 +39,7 @@ def predict():
 if __name__ == "__main__":
     app.run(host='127.0.0.1')
 
-#if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
     
